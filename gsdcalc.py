d58ec5ad27e5c15d394c37a35d8d0517e48f850c
timjascha <timjascha@users.noreply.github.com>
2020-05-17 14:46:56 +0200

    fixed

---
+++ gsdcalc.py
@@ -0,0 +1,61 @@
+from guizero import App, Text, TextBox, Combo, PushButton
+import math as m
+
+def update():
+    if drone.value=="-Custom-":
+        resw.value="0"
+        resh.value="0"
+        sensw.value="0"
+        sensh.value="0"
+        pcount.value="0"
+        psize.value="0"
+        fole.value="0"
+    elif drone.value=="DJI Inspire 1 - Zenmuse X5":
+        resw.value = "4096"
+        resh.value = "2160"
+        sensw.value = "17.3"
+        sensh.value = "13"
+        fole.value = "15"
+
+def calcgsd():
+    x=float(gsd_v.value)/100 #gsd_v.value gibt die gewünschte GSD in mm an
+    y=float(fole.value) #fole.value gibt die Brennweite an
+    z=float(psize.value) #psize.value gibt die Pixelgröße an
+    fheight=x*y/z #fheight ist die errechnete Flughöhe
+    gsd_h.value=str(fheight)
+
+def pixelsize():
+    a = float(sensw.value) #sensw.value=Sensorbreite
+    b = float(sensh.value) #sensh.value=Sensorhöhe
+    asens = a * b #asens=Sensorfläche
+    c=float(resh.value)*float(resw.value) #res=Auflösung
+    pcount.value=str(c)
+    apixel = asens / c #Pixelfläche
+    pixels = m.sqrt(apixel) #Kantenlänge Pixel
+    psize.value = str(pixels)
+
+app = App(title="GSD-Berechner", width=550, height=350,layout="grid")
+drone_t = Text(app, text="Drohne:", grid=[0,0])
+drone=Combo(app, options=["-Custom-","DJI Inspire 1 - Zenmuse X5"],grid=[1,0], command=update)
+gsd_vt = Text(app, text="Gewünschte GSD", grid=[0,1])
+gsd_v = TextBox(app, grid=[1,1])
+gsd_ht = Text(app, text="Benötigte Höhe", grid=[0,2])
+gsd_h = TextBox(app, grid=[1,2])
+gsd_c = PushButton(app, text="Flughöhe berechnen", command=calcgsd, grid=[2,2])
+resw_t = Text(app, text="Auflösung breit", grid=[0,3])
+resw = TextBox(app, text="Auflösung breit", grid=[1,3])
+resh_t = Text(app, text="Auflösung hoch", grid=[2,3])
+resh = TextBox(app, text="Auflösung hoch", grid=[3,3])
+sensw_t = Text(app, text="Sensorbreite", grid=[0,4])
+sensw = TextBox(app, text="Sensorbreite", grid=[1,4])
+sensh_t = Text(app, text="Sensorhöhe", grid=[2,4])
+sensh= TextBox(app,text="Sensorhöhe", grid=[3,4])
+fole_t = Text(app, text="Brennweite", grid=[0,5])
+fole = TextBox(app, text="Brennweite", grid=[1,5])
+psize_t = Text(app, text="Brennweite", grid=[0,6])
+psize = TextBox(app, text="Pixelgröße", grid=[1,6])
+pcount_t = Text(app, text="Pixelanzahl", grid=[0,7])
+pcount = TextBox(app, text="Pixelanzahl", grid=[1,7])
+psize_but = PushButton(app, text="Pixelgröße berechnen", command=pixelsize, grid=[2,7])
+
+app.display()
